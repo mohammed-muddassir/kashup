7e0d141e6cf786b125d8cea95f33f8a6d7453306
muddassir <muddassirmud15@gmail.com>
2025-05-21 22:46:18 +0530

    init: Initial Commit

---
+++ src/main/java/com/kashup/KashUp/KashUpApplication.java
@@ -0,0 +1,15 @@
+package com.kashup.KashUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KashUpApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KashUpApplication.class, args);
+		//initial commit
+	}
+
+}
---
+++ src/test/java/com/kashup/KashUp/KashUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashup.KashUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KashUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
